f36962e56762181927c52295e9036dda5b03d479
mec0104 <70182553+mec0104@users.noreply.github.com>
2021-11-07 19:28:54 -0600

    Add files via upload

---
+++ image-type-converter.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 25 18:15:45 2021
+
+@author: Mary
+"""
+from PIL import Image
+
+import os
+directory = (r'path\to\im\dir')
+
+for filename in os.listdir(directory):
+    if filename.endswith(".im_type"): #original image type
+          print(filename)
+          path = os.path.join(directory,filename)
+          print(path)
+          im1 = Image.open(path)
+          im1.save(filename,'im_type') #new image type
+          
+    if filename.endswith(".im_type"): #original image type
+          path = os.path.join(directory,filename)
+          im2 = Image.open(path)
+          im2.save(filename,'im_type') #new image type 
+    
+  
